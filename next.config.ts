44fc5db6bd602f9baa0806073146fdeab374a1c2
drkswg <kodian17@gmail.com>
2026-03-11 18:30:19 +0100

    Fixed e2e for Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const isGithubPagesBuild = process.env.GITHUB_PAGES === 'true';
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
-const basePath = isGithubActions && repoName ? `/${repoName}` : '';
+const basePath = isGithubPagesBuild && repoName ? `/${repoName}` : '';
 
 const nextConfig: NextConfig = {
   output: 'export',
